d41bfa69323f68dcce04c1adcdafb1b1601dd21c
Dair Smith <dair.code@gmail.com>
2022-09-14 20:26:56 -0500

    feature: draws canvas to ascii, reactive slider
    stretch goal functionality put in readme

---
+++ README.md
@@ -0,0 +1,14 @@
+### Img to Ascii art generator
+
+## Future Plans
+
+- Multiple symbol sets
+- - all nums, all letters, kanji, binary
+- Color rules
+- - matrix code rain, black/white
+- Random characters
+- - each symbol set has array of 5 characters per breakpoint
+- Set fonts
+- - sans-serif, monospace, fantasy, google fonts
+- Set font size
+- - 4px, 10px, 16px, 24px, 32px, 36px, 40px
--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
+const exampleImgs = ['images/railway1.jpeg', 'images/madlib1.png', 'images/nintendods.png', 'images/arch-linux.png', 'images/apple1.png'];
+
 const image1 = new Image();
-image1.src = 'images/railway1.jpeg';
+image1.src = exampleImgs[0];
+
+const sizeSlider = document.getElementById('resolution');
+const inputLabel = document.getElementById('resolutionLabel');
+
+sizeSlider.addEventListener('change', () => {
+    handleSlider();
+})
+
+// cell class
+class Cell {
+    constructor(x, y, symbol, color) {
+        this.x = x;
+        this.y = y;
+        this.symbol = symbol;
+        this.color = color;
+    }
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.fillText(this.symbol, this.x, this.y);
+    }
+}
 
 // ascii class and private class fields
 class AsciiEffect {
     #imageCellArray = [];
-    #symbols = [];
     #pixels = [];
     #ctx;
     #width;
@@ -24,31 +46,57 @@ class AsciiEffect {
 
     // averageColorValue passed in as acv
     #convertToSymbol(acv) {
-        if (acv > 250) return '\u003A'; // :
-        else if (acv < 240) return '\u005B'; // [
-        else if (acv < 230) return '\u005D'; // ]
-        else if (acv < 220) return '*'; //
-        else if (acv < 210) return '^'; //
-        else if (acv < 200) return '/'; //
-        else if (acv < 190) return '%'; //
-        else if (acv < 180) return '_'; //
-        else if (acv < 170) return '.'; //
-        else if (acv < 160) return '>'; //
-        else if (acv < 150) return 'L'; //
-        else if (acv < 140) return ';'; //
-        else if (acv < 130) return '0'; //
-        else if (acv < 120) return 'U'; //
-        else if (acv < 110) return 'E'; //
-        else if (acv < 100) return 'G'; //
-        else if (acv < 90) return 'M'; //
-        else if (acv < 80) return 'A'; //
-        else if (acv < 70) return 'O'; //
-        else if (acv < 60) return 'R'; //
-        else if (acv < 50) return 'B'; // 
-        else if (acv < 40) return 'S'; //
-        else if (acv < 30) return '\u00B6'; // ¶
-        else if (acv < 20) return '\u0024'; // $
-        else if (acv < 10) return '\u0023'; // #
+        if (acv > 250) {
+            return '.';
+        } else if (acv > 240) {
+            return '\u005B'; // [
+        } else if (acv > 230) {
+            return '^';
+        } else if (acv > 220) {
+            return '*';
+        } else if (acv > 210) {
+            return '\u005D'; // ]
+        } else if (acv > 200) {
+            return '/';
+        } else if (acv > 190) {
+            return '\u003A'; // :
+        } else if (acv > 180) {
+            return '_';
+        } else if (acv > 170) {
+            return '%';
+        } else if (acv > 160) {
+            return '>';
+        } else if (acv > 150) {
+            return 'L';
+        } else if (acv > 140) {
+            return ';';
+        } else if (acv > 130) {
+            return '0';
+        } else if (acv > 120) {
+            return 'U';
+        } else if (acv > 110) {
+            return 'E';
+        } else if (acv > 100) {
+            return 'G';
+        } else if (acv > 90) {
+            return 'M';
+        } else if (acv > 80) {
+            return '\u00B6'; // ¶
+        } else if (acv > 70) {
+            return 'O';
+        } else if (acv > 60) {
+            return 'R';
+        } else if (acv > 50) {
+            return '\u0024'; // $
+        } else if (acv > 40) {
+            return 'S';
+        } else if (acv > 30) {
+            return 'A';
+        } else if (acv > 20) {
+            return 'B';
+        } else if (acv > 10) {
+            return '\u0023'; // #
+        }
     }
 
     #scanImage(cellSize) {
@@ -67,16 +115,41 @@ class AsciiEffect {
                     const averageColorValue = total / 3;
                     const color = `rgb(${red}, ${green}, ${blue})`;
                     const symbol = this.#convertToSymbol(averageColorValue);
+                    if (total > 30) this.#imageCellArray.push(new Cell(x, y, symbol, color));
                 }
             }
         }
+        console.log(this.#imageCellArray);
+    }
+    #makeAscii() {
+        this.#ctx.clearRect(0, 0, this.#width, this.#height);
+        for (let i = 0; i < this.#imageCellArray.length; i++) {
+            this.#imageCellArray[i].draw(this.#ctx);
+        }
+    }
+    draw(cellSize) {
+        this.#scanImage(cellSize);
+        this.#makeAscii();
     }
 }
 
 let effect;
+
+function handleSlider() {
+    if (sizeSlider.value == 1) {
+        inputLabel.textContent = 'original image';
+        ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
+    } else {
+        inputLabel.textContent = `Resolution: ${sizeSlider.value}px`;
+        // ctx.font = parseInt(sizeSlider.value) * 2 + 'px monospace';
+        effect.draw(parseInt(sizeSlider.value));
+    }
+}
+
 image1.onload = function initialize() {
     canvas.width = image1.width;
     canvas.height = image1.height;
     effect = new AsciiEffect(ctx, image1.width, image1.height);
     console.log(effect);
+    handleSlider();
 }
